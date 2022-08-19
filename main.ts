18c54803a8f589cbc98454860052c67c40232a38
mie998 <nishiwaki.kyoto@gmail.com>
2022-08-20 01:32:37 +0900

    tw style application done 🙌

--- main.ts
+++ main.ts
@@ -3,10 +3,23 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
+import { config, setup } from "@twind";
+import { InnerRenderFunction, RenderContext } from "$fresh/server.ts";
+import { virtualSheet } from "twind/sheets";
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 
-import twindPlugin from "$fresh/plugins/twind.ts";
-import twindConfig from "./twind.config.js";
+const sheet = virtualSheet();
+sheet.reset();
+setup({ sheet, ...config });
 
-await start(manifest, { plugins: [twindPlugin(twindConfig)] });
+export function render(ctx: RenderContext, render: InnerRenderFunction) {
+  const snapshot = ctx.state.get("twind") as unknown[] | null;
+  sheet.reset(snapshot || undefined);
+  render();
+  ctx.styles.splice(0, ctx.styles.length, ...sheet.target);
+  const newSnapshot = sheet.reset();
+  ctx.state.set("twind", newSnapshot);
+}
+
+await start(manifest, { render });
